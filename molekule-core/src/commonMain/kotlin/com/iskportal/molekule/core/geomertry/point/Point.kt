e782a18609ac813ffc378847299e868121871617
human <nort3x911@protonmail.com>
2024-07-15 01:41:13 +0330

    [bugfix] cross product 😥 - sign was incorrect

--- molekule-core/src/commonMain/kotlin/com/iskportal/molekule/core/geomertry/point/Point.kt
+++ molekule-core/src/commonMain/kotlin/com/iskportal/molekule/core/geomertry/point/Point.kt
@@ -26,7 +26,7 @@ data class Point(
     infix fun cross(other: Point): Point =
         Point(
             y * other.z - z * other.y,
-            x * other.z - z * other.x,
+             z * other.x - x * other.z ,
             x * other.y - y * other.x
         )
 
